710137fbdb39c10620ec579bb96d303631aaae07
Alonso121104 <163936924+Alonso121104@users.noreply.github.com>
2026-04-22 15:11:55 -0700

    Update twilio.java

--- twilio.java
+++ twilio.java
@@ -4,19 +4,25 @@ admin.initializeApp();
 
 const twilio = require("twilio");
 
-const client = new twilio("TWILIO_SID", "TWILIO_AUTH");
+// ⚠️ Move these to environment variables later
+const client = new twilio(
+  "AC5faa95af4d7f0d604b64d30a440e71d2",
+  "a90dc78bc2549216c6684572a0b55aec"
+);
 
 exports.sendSMS = functions.firestore
   .document("requests/{id}")
   .onCreate((snap) => {
+
     const data = snap.data();
 
     return client.messages.create({
-      body: `New Request:
-Address: ${data.address}
-Phone: ${data.phone}
-Time: ${data.time}`,
-      from: "YOUR_TWILIO_NUMBER",
-      to: "YOUR_PERSONAL_PHONE"
+      body: `🚨 New Request
+📍 Address: ${data.address}
+📞 Phone: ${data.phone}
+⏰ Time: ${data.time}`,
+
+      from: "+13236010663",
+      to: "+13236134802"
     });
   });
